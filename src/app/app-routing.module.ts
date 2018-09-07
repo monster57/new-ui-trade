0b92673b36108e87f85e302922f9fd5e53211e9c
surajit barman <surajit.barman@sudrania.com>
2018-09-07 23:16:43 +0530

    routing works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component'
+import { SignupComponent } from './signup/signup.component'
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: []
+})
+
+
+const AppRoutes: Routes = [
+	{
+		path: '',
+		component:LoginComponent
+	},
+	{
+		path: 'signup',
+		component:SignupComponent
+	},
+	
+];
+
+
+@NgModule({
+	imports: [
+		RouterModule.forRoot(AppRoutes),
+	],
+	exports: [RouterModule],
 })
 export class AppRoutingModule { }
